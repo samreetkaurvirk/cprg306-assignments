a8a87c42a0046a64edb237678318b02661f96c60
samreetkaurvirk <130794479+samreetkaurvirk@users.noreply.github.com>
2024-03-15 23:59:31 -0600

    Add files via upload

---
+++ app/cprg306-midterm-exam/cprg306-midterm-exam/components/apiCallExam.jsx
@@ -0,0 +1,16 @@
+"use client";
+import React, { useEffect, useState } from "react";
+
+const APICallExam = () => {
+  return (
+    <div>
+      <div className="flex items-center justify-center">
+        <button className="bg-red-100 p-2 rounded-md border border-red-500">
+          Submit
+        </button>
+      </div>
+    </div>
+  );
+};
+
+export default APICallExam;
